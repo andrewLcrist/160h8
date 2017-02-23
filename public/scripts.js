aad4f80c699129c5f79c390c1afddf34b1923722
Andrew Crist <andrew.l.crist@gmail.com>
2017-02-23 13:49:47 -0700

    Add forgiveness button

--- public/scripts.js
+++ public/scripts.js
@@ -21,17 +21,39 @@ $('.submit').click(function(e){
 })
 
 function renderHatred() {
+  clearHatred()
   $.get('/hateList', function(hateList){
     hateList.forEach(function(hated){
+      let forgiveness = hated.forgive == "true" ? "Forgiven" : "Not Forgiven"
       $('.hate-table').append(
         `  <tr>
             <td>Offender: ${hated.name}</td>
             <td>Offense: ${hated.offense}</td>
             <td>Date of Offense: ${hated.date}</td>
-            <td>Forgiven? ${hated.forgive}</td>
+            <td>Forgiven? <button class="toggle-hatred" onclick="toggleForgiveness(${hated.id})" >${forgiveness}</button></td>
           </tr>
         `
       )
+    })
+  })
+}
+
+function clearHatred() {
+  $('.hate-table').empty()
+}
+
+function toggleForgiveness(id) {
+  $.get(`/hateList/${id}`, function(response) {
+    let toggle = response.hateList.forgive == "false" ? true : false
+    $.ajax({
+      type: "PUT",
+      url: `/hateList/${id}`,
+      data: {
+        forgive: toggle
+      }
+    })
+    .then(function() {
+      renderHatred()
     })
   })
 }
